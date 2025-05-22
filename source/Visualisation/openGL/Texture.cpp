6cee1a91329a906021cecd0a0dbb70022b47e2ee
Gabriel Zerbib <moddingear@gmail.com>
2025-05-22 23:51:56 +0200

    debug opengl

--- source/Visualisation/openGL/Texture.cpp
+++ source/Visualisation/openGL/Texture.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+#define gl_check_err() {GLenum stoerr = glGetError(); if(stoerr != GL_NO_ERROR) {cout << "@ line " << __LINE__ << " error " << gluErrorString(stoerr) <<endl; assert(0);}}
+
 Texture::~Texture()
 {
 	if (TextureID)
@@ -52,25 +54,31 @@ void Texture::LoadFromUMat(const cv::UMat &Image)
 
 void Texture::Bind()
 {
+	glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+	gl_check_err();
 	glGenTextures(1, &TextureID);
+	gl_check_err();
 	LastSize = cv::Size(0,0);
+	//cout << "glGenTextures " << TextureID << " error " << gluErrorString(glGetError()) << endl;
 
 	Refresh();
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	gl_check_err();
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	gl_check_err();
 	
-	cout << "Created texture " << TextureID << endl;
 }
 
 void Texture::Refresh()
 {
 	glBindTexture(GL_TEXTURE_2D, TextureID);
-
+	gl_check_err();
 	cv::Size NewSize = SourceImage.size();
 	int numchannels = SourceImage.channels();
 	GLenum format = numchannels == 3 ? GL_BGR : GL_R;
 	GLint internal_format = numchannels == 3 ? GL_RGB : GL_R;
+	//cout << "Bound to " << TextureID << " @ " << this << ", resolution " << NewSize << endl;
 	if ((NewSize.width*numchannels) % 4)
 	{
 		cout << "Warning : Texture width (" << NewSize.width << "*" << numchannels << ") not multiple of 4, misaligned!" << endl;
@@ -79,13 +87,19 @@ void Texture::Refresh()
 	if (NewSize != LastSize)
 	{
 		assert(LastSize == cv::Size());
+		glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+		gl_check_err();
 		glTexImage2D(GL_TEXTURE_2D, 0, internal_format, NewSize.width, NewSize.height, 0, format, GL_UNSIGNED_BYTE, SourceImage.data);
+		gl_check_err();
+		//cout << "glTexImage2D " << TextureID << " error " << gluErrorString(glGetError()) << endl;
 	}
 	else
 	{
 
 		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, NewSize.width, NewSize.height, format, GL_UNSIGNED_BYTE, SourceImage.data);
+		gl_check_err();
 	}
+	//cout << "Texture " << TextureID << " done updating" << endl;
 	LastSize = NewSize;
 }
 
